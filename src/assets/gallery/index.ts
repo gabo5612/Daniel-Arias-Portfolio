699c886313dfe9e08f3df7a99cce25fa34e414e1
Gabo <gabo5612@gmail.com>
2026-05-27 23:16:12 -0400

    refactor: remove unused gallery images and update imports

--- src/assets/gallery/index.ts
+++ src/assets/gallery/index.ts
@@ -1,36 +1,15 @@
-import img01 from './_ADA3397.webp';
-import img02 from './_ADA3429.webp';
-import img03 from './_ADA3478-2.webp';
-import img04 from './_ADA3478.webp';
-import img05 from './_ADA3517-2.webp';
-import img06 from './_ADA3517.webp';
-import img07 from './_ADA3551.webp';
-import img08 from './_ADA3687.webp';
-import img09 from './_ADA3785.webp';
-import img10 from './_ADA3877.webp';
-import img11 from './image00001.webp';
-import img12 from './image00003.webp';
-import img13 from './image00005.webp';
-import img14 from './image00008.webp';
-import img15 from './image00009.webp';
-import img16 from './image00010.webp';
-import img17 from './image00011.webp';
-import img18 from './image00012.webp';
-import img19 from './image00013.webp';
-import img20 from './image00018.webp';
-import img21 from './image00019.webp';
-import img22 from './image00022.webp';
-import img23 from './image00023.webp';
-import img24 from './image00025.webp';
-import img25 from './image00027.webp';
-import img26 from './image00029.webp';
-import img27 from './image00032.webp';
-import img28 from './image00034.webp';
-import img29 from './image00035.webp';
-import img30 from './image00036.webp';
+import img1 from "./_ADA3429.webp"
+import img2 from "./_ADA3478.webp"
+import img3 from "./_ADA3877.webp"
+import img4 from "./image00003.webp"
+import img5 from "./image00005.webp"
+import img6 from "./image00010.webp"
+import img7 from "./image00011.webp"
+import img8 from "./image00012.webp"
+import img9 from "./image00013.webp"
+import img10 from "./image00032.webp"
+
 
 export const galleryImages = [
-  img01, img02, img03, img04, img05, img06, img07, img08, img09, img10,
-  img11, img12, img13, img14, img15, img16, img17, img18, img19, img20,
-  img21, img22, img23, img24, img25, img26, img27, img28, img29, img30,
+  img1, img2, img3, img4, img5, img6, img7, img8, img9, img10
 ];
